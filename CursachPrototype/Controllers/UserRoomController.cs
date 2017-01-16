2b78a12bbfc930b6ec0a70597f641928aba8d2eb
genri1996 <genri1996@mail.ru>
2017-01-16 23:40:44 +0200

    added top user bar

---
+++ CursachPrototype/Controllers/UserRoomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursachPrototype.Controllers
+{
+    public class UserRoomController : Controller
+    {
+        // GET: UserRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
