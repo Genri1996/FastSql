24c3f01389996180812886d6af12df46d78456fb
genri1996 <genri1996@mail.ru>
2017-01-27 15:16:25 +0200

    Installed Web.Optimization, Used UIrl.Content instead of raw references

---
+++ CursachPrototype/App_Start/BundleConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Optimization;
+
+namespace CursachPrototype
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles) { }//TODO Зачем?
+    }
+}
