b292b9e1e63e07cc5f693512b25134c2bb6c54ad
genri1996 <genri1996@mail.ru>
2017-02-10 21:31:20 +0200

    Implemented part of editing

--- CursachPrototype/Controllers/DisplayTableController.cs
+++ CursachPrototype/Controllers/DisplayTableController.cs
@@ -5,29 +5,57 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursachPrototype.Models.Accounting;
+using CursachPrototype.ViewModels;
 using DataProxy.DataBaseReaders;
+using DataProxy.DbManangment;
+using Microsoft.AspNet.Identity;
 
 namespace CursachPrototype.Controllers
 {
     public class DisplayTableController : Controller
     {
+        private DataTable _model;
+
+        public DisplayTableController()
+        {
+            _model = new DataTable("Table");
+            _model.Columns.Add(new DataColumn("ID", typeof(int)));
+            _model.Columns.Add(new DataColumn("TestStr", typeof(string)));
+            _model.Rows.Add(55, "TestString");
+
+
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
-            var cs = ConfigurationManager.ConnectionStrings["IdentityDbOleDb"].ConnectionString;
-            var dt = new OleDbDataBaseReader(cs).LoadTables("DbInfos").Tables["DbInfos"];
+            //var cs = ConfigurationManager.ConnectionStrings["IdentityDbOleDb"].ConnectionString;
+            //_model = new OleDbDataBaseReader(cs).LoadTables("DbInfos").Tables["DbInfos"];
 
-            return View(dt);
+            return View(_model);
         }
 
         [HttpPost]
-        public ActionResult UploadChanges(DataTable dt)
+        public ActionResult UploadChanges()
         {
-            return null;
+
+            return PartialView("_Message", "Успех");
         }
 
         [HttpGet]
         public ActionResult Edit(int id)
+        {
+            DataRow dr = (from DataRow dataRow in _model.Rows
+                          let idOfCurrentRow = int.Parse(dataRow[_model.Columns["ID"]].ToString())
+                          where idOfCurrentRow == id
+                          select dataRow).FirstOrDefault();
+
+            return PartialView("~/Views/DisplayTable/EditRow.cshtml", dr);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
         {
             return null;
         }
